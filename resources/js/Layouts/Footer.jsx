f01bf4bbb54799afe509fb4c819c8a419eac2a52
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-04-10 21:42:15 +0700

    feat: add Footer component for site footer section

---
+++ resources/js/Layouts/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-6">
+            <p className="text-center">&copy; 2025 Your Company. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
