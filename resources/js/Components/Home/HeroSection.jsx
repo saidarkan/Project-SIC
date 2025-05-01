a9ded28ead3005e5e4d4642005f97fa85abf4f4b
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-01 15:31:20 +0700

    feat: clean up HeroSection by removing commented code and improving image structure

--- resources/js/Components/Home/HeroSection.jsx
+++ resources/js/Components/Home/HeroSection.jsx
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-
 export default function HeroSection() {
     const ref = useRef(null);
 
@@ -19,14 +18,18 @@ export default function HeroSection() {
             ref={ref}
             className="min-h-screen bg-gradient-to-b from-lime-600 via-lime-700 to-lime-900 flex items-center justify-center relative overflow-hidden"
         >
-            {/* Gambar background parallax */}
             <motion.img
+                src="/build/images/about-us.png"
+                className="absolute inset-0 w-full h-full object-cover "
+                style={{ y }}
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-black/80 z-0" />
+            {/* <motion.img
                 src="/build/images/forest.png"
                 className="absolute inset-0 w-full h-full object-cover"
                 style={{ y }}
-            />
+            /> */}
 
-            {/* Gambar karakter animasi naik-turun */}
             <motion.img
                 src="/build/images/gambi-hero.png"
                 alt="gambi-hero.png"
