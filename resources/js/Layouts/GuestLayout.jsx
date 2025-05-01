294fd2e98c68275ef2609afce93853dffda83b1f
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-01 15:32:29 +0700

    feat: refactor GuestLayout to improve structure and restore Footer component

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -6,11 +6,20 @@ import Footer from "@/Layouts/Footer";
 export default function GuestLayout({ children }) {
     return (
         <>
-            <div className="flex min-h-screen flex-col ">
+            {/* <div className="min-h-screen bg-gradient-to-b from-lime-900 via-lime-800 to-lime-700 text-white">
                 <Navbar />
                 {children}
-                {/* Footer */}
-                {/* <Footer /> */}
+                <Footer />
+            </div> */}
+
+            <div className="bg-gradient-to-b from-lime-900 via-lime-800 to-lime-700 min-h-screen flex">
+                <div className="flex flex-col flex-1">
+                    <Navbar />
+                    <div className="flex-1">
+                        {children}
+                    </div>
+                    <Footer />
+                </div>
             </div>
         </>
     );
