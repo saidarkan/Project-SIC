2c550dad5d9460e2130597e21edb1968de00631b
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-01 19:05:34 +0700

    feat: remove gradient background from GuestLayout for a cleaner design

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -12,7 +12,7 @@ export default function GuestLayout({ children }) {
                 <Footer />
             </div> */}
 
-            <div className="bg-gradient-to-b from-lime-900 via-lime-800 to-lime-700 min-h-screen flex">
+            <div className=" min-h-screen flex">
                 <div className="flex flex-col flex-1">
                     <Navbar />
                     <div className="flex-1">
