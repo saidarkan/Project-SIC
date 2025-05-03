0c054a742b58dd6b7e693ea2013b37923b921b3c
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-03 17:14:43 +0700

    fix: adjust margin-top in Home component for better layout alignment

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     return (
         <>
             <GuestLayout>
-                <div className="-mt-[8.5rem]">
+                <div className="-mt-[8rem]">
                     <HeroSection />
                     <ScrollButtons
                         onTop={scrollToTop}
