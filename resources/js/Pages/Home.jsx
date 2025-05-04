ff5e14382f1ca4f423646b79d3354cb95d7e885a
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-04 21:15:12 +0700

    refactor: clean up imports and improve formatting in Home component

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,20 +1,19 @@
 // Importing React and useRef from React library
 import React, { useRef } from "react";
-import { Head} from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
 // Componen utama untuk halaman Home
 import GuestLayout from "@/Layouts/GuestLayout";
 import HeroSection from "@/Components/Home/HeroSection";
 import ScrollButtons from "@/Components/Home/ScrollButtons";
 import AboutSection from "@/Components/Home/AboutSection";
-
+import PeatFunctionsSection from "@/Components/Home/PeatFunctionsSection";
+import PeatJourneySection from "@/Components/Home/PeatJourneySection";
 const Home = () => {
     const bottomRef = useRef(null);
+    const peatJourneyRef = useRef(null); // khusus untuk PeatJourneySection
 
     const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
+        window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
     const scrollToBottom = () => {
@@ -22,20 +21,25 @@ const Home = () => {
     };
 
     return (
-        <>
-            <GuestLayout>
-                  <Head title="Beranda" />
-                <div className="-mt-[8rem]">
-                    <HeroSection />
-                    <ScrollButtons
-                        onTop={scrollToTop}
-                        onBottom={scrollToBottom}
-                    />
-                    <AboutSection ref={bottomRef} />
-                </div>
-            </GuestLayout>
-        </>
+        <GuestLayout>
+            <Head title="Beranda" />
+            <div className="-mt-[8rem]">
+                <HeroSection />
+                <ScrollButtons
+                    onTop={scrollToTop}
+                    onBottom={scrollToBottom}
+                />
+
+                <AboutSection />
+
+                <PeatFunctionsSection ref={bottomRef} />
+
+                {/* Gunakan ref yang berbeda */}
+                <PeatJourneySection ref={peatJourneyRef} />
+            </div>
+        </GuestLayout>
     );
 };
+
 
 export default Home;
