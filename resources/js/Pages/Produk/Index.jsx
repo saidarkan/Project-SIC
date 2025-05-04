f592d997c54d72d9b44f4da8fe149b5e72904e54
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-04 15:38:50 +0700

    fix: streamline scrollToTop function and adjust layout in Produk Index component

--- resources/js/Pages/Produk/Index.jsx
+++ resources/js/Pages/Produk/Index.jsx
@@ -2,15 +2,13 @@ import React, { useRef } from "react";
 import GuestLayout from "@/Layouts/GuestLayout";
 import ScrollButtons from "@/Components/Home/ScrollButtons";
 import Carousel from "@/Components/Produk/Carousel";
+import ProdukCard from "@/Components/Produk/ProdukCard";
 
 const Index = () => {
     const bottomRef = useRef(null);
 
     const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
+        window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
     const scrollToBottom = () => {
@@ -22,7 +20,10 @@ const Index = () => {
             <div className="mt-0">
                 <Carousel />
                 <ScrollButtons onTop={scrollToTop} onBottom={scrollToBottom} />
-                <div ref={bottomRef}></div>
+
+                <div ref={bottomRef} className="mt-10"></div>
+
+                <ProdukCard />
             </div>
         </GuestLayout>
     );
