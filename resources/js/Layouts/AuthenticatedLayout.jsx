d02e4b26a42cf753963ff2b26f0e2195c31daf6f
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-24 20:50:32 +0700

    feat: implement sidebar toggle functionality in AuthenticatedLayout component

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import Header from "@/Components/Admin/Layouts/Header";
 import Sidebar from "@/Components/Admin/Layouts/Sidebar";
 
 export default function AuthenticatedLayout({ children }) {
+     const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setSidebarOpen(true);
+    };
+
+    const handleCloseSidebar = () => {
+        setSidebarOpen(false);
+    };
+
     return (
-        <div className="bg-gray-100 min-h-screen flex">
-            <div className="flex flex-row flex-1">
-                <Sidebar />
-                <div className="flex-1 p-4">
-                    <Header />
-                    <main>{children}</main>
-                </div>
+       <div className="flex">
+            <Sidebar isOpen={isSidebarOpen} onClose={handleCloseSidebar} />
+            <div className="flex-1 ml-0 md:ml-72 transition-all duration-300">
+                <Header onMenuClick={handleMenuClick} isSidebarOpen={isSidebarOpen} />
+                <main className="p-4">{children}</main>
             </div>
         </div>
     );
