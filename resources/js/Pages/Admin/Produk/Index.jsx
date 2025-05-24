7eb0aa6dde903c32ebe7011f2faaa8d60cbbadbe
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-24 19:30:20 +0700

    refactor: update Produk page structure and improve layout integration

--- resources/js/Pages/Admin/Produk/Index.jsx
+++ resources/js/Pages/Admin/Produk/Index.jsx
@@ -1,17 +1,28 @@
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-
-export default function Produk() {
+import React, { useRef } from "react";
+import { Head } from "@inertiajs/react";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+export default function Index() {
     return (
-        <div>
-             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <AuthenticatedLayout
+        // header={
+        //     <h2 className="text-xl font-semibold leading-tight text-gray-800">
+        //         Dashboard
+        //     </h2>
+        // }
+        >
+            <Head title="Produk" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                       <h1 className="text-2xl font-bold">Admin Produk</h1>
+                        <div className="p-6 text-gray-900">
+                          Ini Halaman Admin Produk
+                        </div>
                     </div>
                 </div>
-
-        </div>
+            </div>
+        </AuthenticatedLayout>
     );
 }
 
-// Importing the layout for the page
-Produk.layout = (page) => <AuthenticatedLayout>{page}</AuthenticatedLayout>;
+
