f3211f139ff318c3c237f214252cc7430bfc926d
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-05-25 09:27:22 +0700

    feat: add Suspense fallback to improve loading experience in Inertia app

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 
+import React, { Suspense } from 'react';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
@@ -12,12 +13,16 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
-            import.meta.glob('./Pages/**/*.jsx'),
+            import.meta.glob('./Pages/**/*.jsx') // <- ini sudah lazy load
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+                <App {...props} />
+            </Suspense>
+        );
     },
     progress: {
         color: '#4B5563',
